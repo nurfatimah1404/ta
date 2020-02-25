13699bcd7ab899cdefa5094a153265e9468d1303
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-02-26 03:48:44 +0800

    tes

---
+++ tes2.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import paho.mqtt.client as mqtt
+import requests
+import time
+import json
+from datetime import datetime
+from influxdb import InfluxDBClient
+# set influxDB configuration -----------------------------
+dbhost = "10.0.12.127"
+dbport = 8086
+dbuser = ""
+dbpassword = ""
+dbname = "mydb"
+#---------------------------------------------------
+# set mqtt configuration ===========================
+mqtt_server = "10.0.12.127"
+mqtt_port = 1883
+mqtt_user = ""
+mqtt_password = ""
+# =================================================
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+# set client subscriber ----------------------
+    client.subscribe("bme/humidity")
+    client.subscribe("bme/temperature_F")
+    client.subscribe("bme/temperature_C")
+    client.subscribe("bme/pressure")
+    client.subscribe("rainsensor/rainfall")
+    client.subscribe("Wind/wind_direction")
+    client.subscribe("Wind/wind_speed")
+# panel surya---------------------------------
+    client.subscribe("Cap/Bateray")
+    client.subscribe("RELAY/Relay")
+    client.subscribe("BH1750/Light")
+    client.subscribe("TEMPERATUR/celcius")
+    client.subscribe("INA219/busvoltage_C")
+    client.subscribe("INA219/shuntvoltage_C")
+    client.subscribe("INA219/current_mA_C")
+    client.subscribe("INA219/power_mW_C")
+    client.subscribe("INA219/loadvoltage_C")
+    client.subscribe("INA219/busvoltage_B")
+    client.subscribe("INA219/shuntvoltage_B")
+    client.subscribe("INA219/current_mA_B")
+    client.subscribe("INA219/power_mW_B")
+    client.subscribe("INA219/loadvoltage_B")
+#----------------------------------------------
+def on_message(client, userdata, msg):
+    message = str(msg.payload)
+    if is_json(message): 
+        jsonResponse=json.loads(message)
+        print jsonResponse
