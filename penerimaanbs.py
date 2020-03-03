22b33f4f637c8d4d84e36d11bcbe728b43516b6c
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-03-03 10:49:04 +0800

    first

--- penerimaanbs.py
+++ penerimaanbs.py
@@ -1,183 +1,194 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# forwarder.py - forwards IoT sensor data from MQTT to InfluxDB
-# Copyright (C) 2016 Michael Haas <haas@computerlinguist.org>
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-
-import argparse
+#!/usr/bin/env python3
+#!/usr/bin/env python3
 import paho.mqtt.client as mqtt
+import requests
+import StringIO
+import time
+import os
+from datetime import datetime
 from influxdb import InfluxDBClient
-import json
-import re
-import logging
-import sys
-import requests.exceptions
+#====================================================
+# set influxdb configuration -------------------------
+dbhost = "10.0.12.127"
+dbport = 8086
+dbuser = "admin"
+dbpassword = "123456"
+dbname = "mydb"
 
-class MessageStore(object):
-    def store_msg(self, node_name, measurement_name, value):
-        raise NotImplementedError()
+#====================================================
+# set mqtt configuration ===========================
+mqtt_server = "10.0.12.127"
+mqtt_port = 1883
+mqtt_user = "admin"
+mqtt_password = "123456"
+# =================================================    
+#====================================================
 
-class InfluxStore(MessageStore):
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+# set client subscriber ----------------------
+    client.subscribe("temperature)
+    client.subscribe("humidity")
+    client.subscribe("PM_2.5")
+    client.subscribe("NH")  
+    client.subscribe("NO2")  
+    client.subscribe("CO2")
+    client.subscribe("CO")
+    client.subscribe("Asap")
+        
+#    client.subscribe("demo")
+#====================================================
+def on_message(client, userdata, msg):
+    print("Received a message on topic: " + msg.topic)
+    # Use utc as timestamp
+    now = datetime.now()
+    receiveTime = now.strftime("%Y-%m-%d %H:%M:%S")
+    #receiveTime=datetime.datetime.utcnow()
+    message=msg.payload.decode("utf-8")
+    isfloatValue=False    
 
-    logger = logging.getLogger("forwarder.InfluxStore")
+    try:
+        # Convert the string to a float so that it is stored as a number and not a string in the database
+        val = float(message)
+        isfloatValue=True
+    except:
+        print("Could not convert " + message + " to a float value")
+        isfloatValue=False
 
-    def __init__(self, host, port, username, password_file, database):
-        password = open(password_file).read().strip()
-        self.influx_client = InfluxDBClient(
-            host=host, port=port, username=username, password=password, database=database)
-        # influx_client.create_database('sensors')
+    if isfloatValue:
+        print(str(receiveTime) + ": " + msg.topic + " " + str(val))
+            
+        if (msg.topic == "temperature"):            
+            json_data = [
+                {
+                    "measurement": msg.topic,
+                    "tags": {
+                        "time": str(receiveTime),
+                        "id": "123456"
+                    },                    
+                    "fields": {
+                        "temperature": val
+                    }
+                }
+            ]
+            
+        if (msg.topic == "humidity"):              
+            json_data = [
+                {
+                    "measurement": msg.topic,
+                    "tags": {
+                        "time": str(receiveTime),
+                        "id": "123456"
+                    },                    
+                    "fields": {
+                        "humidity": val
+                    }
+                }
+            ]
+        if (msg.topic == "PM_2.5"):                   
+            json_data = [
+                {
+                    "measurement": msg.topic,
+                    "tags": {
+                        "time": str(receiveTime),
+                        "id": "123456"
+                    },                    
+                    "fields": {
+                        "PM_2.5": val
+                    }
+                }
+            ]
+        if (msg.topic == "NH"):                   
+            json_data = [
+                {
+                    "measurement": msg.topic,
+                    "tags": {
+                        "time": str(receiveTime),
+                        "id": "123456"
+                    },                    
+                    "fields": {
+                        "NH": val
+                    }
+                }
+            ]
+        if (msg.topic == "N02"):                   
+            json_data = [
+                {
+                    "measurement": msg.topic,
+                    "tags": {
+                        "time": str(receiveTime),
+                        "id": "123456"
+                    },                    
+                    "fields": {
+                        "NO2": val
+                    }
+                }
+            ]
+        if (msg.topic == "CO2"):                   
+            json_data = [
+                {
+                    "measurement": msg.topic,
+                    "tags": {
+                        "time": str(receiveTime),
+                        "id": "123456"
+                    },                    
+                    "fields": {
+                        "CO2": val
+                    }
+                }
+            ]
+        if (msg.topic == "CO"):                   
+            json_data = [
+                {
+                    "measurement": msg.topic,
+                    "tags": {
+                        "time": str(receiveTime),
+                        "id": "123456"
+                    },                    
+                    "fields": {
+                        "CO": val
+                    }
+                }
+            ]
+        if (msg.topic == "Asap"):                   
+            json_data = [
+                {
+                    "measurement": msg.topic,
+                    "tags": {
+                        "time": str(receiveTime),
+                        "id": "123456"
+                    },                    
+                    "fields": {
+                        "Asap": val
+                    }
+                }
+            ]
+       # saveData(msg.topic, message)
+        #print("Finished writing to File !!!") 
+        #dbClient.create_database(dbname)
+        dbclient.write_points(json_data)
+        print("Finished writing to InfluxDB")
+        print("Waiting data from client...")
+#   client.publish("demo")
+#====================================================        
+# Set up a client for InfluxDB
+dbclient = InfluxDBClient(dbhost, dbport, dbuser, dbpassword, dbname)
 
+#====================================================
+# Initialize the MQTT client that should connect to the Mosquitto broker
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+connOK=False
 
-    def store_msg(self, node_name, measurement_name, data):
-        if not isinstance(data, dict):
-            raise ValueError('data must be given as dict!')
-        influx_msg = {
-            'measurement': measurement_name,
-            'tags': {
-                'sensor_node': node_name,
-            },
-            'fields': data
-        }
-        self.logger.debug("Writing InfluxDB point: %s", influx_msg)
-        try:
-            self.influx_client.write_points([influx_msg])
-        except requests.exceptions.ConnectionError as e:
-            self.logger.exception(e)
-
-class MessageSource(object):
-
-    def register_store(self, store):
-        if not hasattr(self, '_stores'):
-            self._stores = []
-        self._stores.append(store)
-
-    @property
-    def stores(self):
-        # return copy
-        return list(self._stores)
-
-
-class MQTTSource(MessageSource):
-
-    logger = logging.getLogger("forwarder.MQTTSource")
-
-    def __init__(self, host, port, node_names, stringify_values_for_measurements):
-        self.host = host
-        self.port = port
-        self.node_names = node_names
-        self.stringify = stringify_values_for_measurements
-        self._setup_handlers()
-
-    def _setup_handlers(self):
-        self.client = mqtt.Client()
-
-        def on_connect(client, userdata, flags, rc):
-            self.logger.info("Connected with result code  %s", rc)
-            # subscribe to /node_name/wildcard
-            for node_name in self.node_names:
-                topic = "/{node_name}/#".format(node_name=node_name)
-                self.logger.info(
-                    "Subscribing to topic %s for node_name %s", topic, node_name)
-                client.subscribe(topic)
-
-        def on_message(client, userdata, msg):
-            self.logger.debug("Received MQTT message for topic %s with payload %s", msg.topic, msg.payload)
-            token_pattern = ur '(?:\w|-|\.)+'
-            regex = re.compile(
-                ur'/(?P<node_name>' + token_pattern + ')/(?P<measurement_name>' + token_pattern + ')/?')
-            match = regex.match(msg.topic)
-            if match is None:
-                self.logger.warn("Could not extract node name or measurement name from topic %s", msg.topic)
-                return
-            node_name = match.group('node_name')
-            if node_name not in self.node_names:
-                self.logger.warn(
-                    "Extract node_name %s from topic, but requested to receive messages for node_name %s", node_name, self.node_name)
-            measurement_name = match.group('measurement_name')
-
-            value = msg.payload
-
-            is_value_json_dict = False
-            try:
-                stored_message = json.loads(value)
-                is_value_json_dict = isinstance(stored_message, dict)
-            except ValueError:
-                pass
-
-            if is_value_json_dict:
-                for key in stored_message.keys():
-                    try:
-                        stored_message[key] = float(stored_message[key])
-                    except ValueError:
-                        pass
-            else:
-                # if message is not a JSON DICT, only then check if we should stringify the value
-                if measurement_name in self.stringify:
-                    value = str(value)
-                else:
-                    try:
-                        value = float(value)
-                    except ValueError:
-                        pass
-                stored_message = {'value': value}
-
-            for store in self.stores:
-                store.store_msg(node_name, measurement_name, stored_message)
-
-        self.client.on_connect = on_connect
-        self.client.on_message = on_message
-
-    def start(self):
-        self.client.connect(self.host, self.port)
-        # Blocking call that processes network traffic, dispatches callbacks and
-        # handles reconnecting.
-        # Other loop*() functions are available that give a threaded interface and a
-        # manual interface.
-        self.client.loop_forever()
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description='MQTT to InfluxDB bridge for IOT data.')
-    parser.add_argument('--mqtt-host', required=True, help='MQTT host')
-    parser.add_argument('--mqtt-port', default="1883", help='MQTT port')
-    parser.add_argument('--influx-host', required=True, help='InfluxDB host')
-    parser.add_argument('--influx-port', default="8086", help='InfluxDB port')
-    parser.add_argument('--influx-user', required=True,
-                        help='InfluxDB username')
-    parser.add_argument('--influx-pass-file', required=True,
-                        help='InfluxDB password file')
-    parser.add_argument('--influx-db', required=True, help='InfluxDB database')
-    parser.add_argument('--node-name', required=True,
-                        help='Sensor node name', action="append")
-    parser.add_argument('--stringify-values-for-measurements', required=False,
-                        help='Force str() on measurements of the given name', action="append")
-    parser.add_argument('--verbose', help='Enable verbose output to stdout',
-                        default=False, action='store_true')
-    args = parser.parse_args()
-
-    if args.verbose:
-        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-    else:
-        logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-
-    store = InfluxStore(host=args.influx_host, port=args.influx_port,
-            username=args.influx_user, password_file=args.influx_pass_file, database=args.influx_db)
-    source = MQTTSource(host=args.mqtt_host,
-                        port=args.mqtt_port, node_names=args.node_name,
-                        stringify_values_for_measurements=args.stringify_values_for_measurements)
-    source.register_store(store)
-    source.start()
-
-if __name__ == '__main__':
-    main()
+while(connOK == False):
+    try:
+        client.username_pw_set(mqtt_user, mqtt_password)
+        client.connect(mqtt_server, mqtt_port, 60)
+        connOK = True
+    except:
+        connOK = False
+    time.sleep(10)
+#====================================================
+# Blocking loop to the Mosquitto broker
+client.loop_forever()
