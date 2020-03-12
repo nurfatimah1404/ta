12f1f2dbf07bd8a0d1e82221f74c30bb2c3928ba
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-03-12 16:43:07 +0800

    comment

--- olahdata.py
+++ olahdata.py
@@ -1,5 +1,6 @@
 from influxdb import DataFrameClient
 
 client = DataFrameClient(host='10.0.12.127', port=8086, username='admin', password='123456', database='NOAA_water_database')
-daata = "select * from h2o_feet where time > now() - 1h"
-select * from h2o_feet where time > now() - 1h
+data = "select * from h2o_feet where time > now() - 1h"
+print data
+#select * from h2o_feet where time > now() - 1h
