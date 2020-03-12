691dded42a5a60da0e7d35c7d218c1675a44939b
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-03-12 16:51:30 +0800

    import pandas

--- olahdata.py
+++ olahdata.py
@@ -1,4 +1,5 @@
 import influxdb
+import pandas as pd   
 dfclient = influxdb.DataFrameClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_dataabase')
 q = "select * from h2o_feet"
 df = pd.DataFrame(client.query(q, chunked=True, chunk_size=10000).get_points())  # Returns all points
