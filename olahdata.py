66669f28e843600336946923fb2667cba8ca856e
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-03-13 14:21:51 +0800

    ubah script

--- olahdata.py
+++ olahdata.py
@@ -1,5 +1,6 @@
-from influxdb import InfluxDBClient
-import pandas as pd   
+import pandas as pd
+from influxdb import InfluxDBClient   
 dfclient = influxdb.DataFrameClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_dataabase')
 q = "select * from h2o_feet"
-df = pd.DataFrame(client.query(q, chunked=True, chunk_size=10000).get_points())  # Returns all points
+df = pd.DataFrame(client.query('SELECT * FROM "h2o_feet" LIMIT 2'))
+#client.query('SELECT "duration" FROM "pyexample"."autogen"."brushEvents" WHERE time > now() - 4d GROUP BY "user"')
