4a8a0039bf05ab513e515718bf5c7b295be8ace1
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-03-13 14:43:43 +0800

    ubah script

--- olahdata.py
+++ olahdata.py
@@ -4,7 +4,7 @@ from influxdb import InfluxDBClient
 #client = InfluxDBClient('localhost', 8086, 'root', 'root', 'example')
 client = InfluxDBClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_database')
 #client = InfluxDBClient(host='mydomain.com', port=8086, username='myuser', password='mypass' ssl=True, verify_ssl=True)
-#q = "select * from h2o_feet LIMIT 2"
-#df = pd.DataFrame(client.query(q, chunked=True, chunk_size=10000).get_points())
-result = client.query('SELECT * FROM h2o_feet LIMIT 2')
-print (result)
+q = "select * from h2o_feet LIMIT 2"
+df = pd.DataFrame(client.query(q, chunked=True, chunk_size=10000).get_points())
+#result = client.query('SELECT * FROM h2o_feet LIMIT 2')
+#print (result)
