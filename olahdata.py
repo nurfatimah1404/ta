7cc473a9b8d006110f7b780c53a179c4062cec4b
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-03-15 04:33:06 +0800

    ubah query

--- olahdata.py
+++ olahdata.py
@@ -4,7 +4,7 @@ from influxdb import InfluxDBClient
 #client = InfluxDBClient('localhost', 8086, 'root', 'root', 'example')
 client = InfluxDBClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_database')
 #client = InfluxDBClient(host='mydomain.com', port=8086, username='myuser', password='mypass' ssl=True, verify_ssl=True)
-q = "SELECT MEAN("water_level") FROM "h2o_feet" GROUP BY time(1h)"
+q = "SELECT MEAN("water_level") FROM "h2o_feet" GROUP BY time(1h) LIMIT 8"
 #SELECT MEAN("water_level") FROM "h2o_feet" GROUP BY "location"
 df = pd.DataFrame(client.query(q, chunked=True, chunk_size=10000).get_points())
 print (df)
