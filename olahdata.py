0f3e4af9ba7b5e5b54ad53bde94814d1c0d27855
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-04-09 03:28:29 +0800

    ubh

--- olahdata.py
+++ olahdata.py
@@ -7,6 +7,7 @@ dbport = 8086
 dbuser = "admin"
 dbpassword = "123456"
 dbname = "mydb"
+dbclient = InfluxDBClient(dbhost, dbport, dbuser, dbpassword, dbname)
 client = InfluxDBClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_database')
 #client = InfluxDBClient(host='mydomain.com', port=8086, username='myuser', password='mypass' ssl=True, verify_ssl=True)
 q = "SELECT MEAN(water_level) FROM h2o_feet GROUP BY time(1h) LIMIT 8"
@@ -26,6 +27,5 @@ json_body = [
             }
         ]
 dbclient.write_points(json_body)
-dbclient = InfluxDBClient(dbhost, dbport, dbuser, dbpassword, dbname)
 print("Finished writing to InfluxDB")
 print (json_body)
