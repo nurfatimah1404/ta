9b5a1f0c8ec2bee11b096e9d849331e4da0ae85e
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-04-09 03:26:40 +0800

    tmbh

--- olahdata.py
+++ olahdata.py
@@ -6,13 +6,12 @@ dbhost = "10.0.12.127"
 dbport = 8086
 dbuser = "admin"
 dbpassword = "123456"
-dbname = "NOAA_water_database'"
-dbclient = InfluxDBClient(dbhost, dbport, dbuser, dbpassword, dbname)
-#client = InfluxDBClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_database')
+dbname = "mydb"
+client = InfluxDBClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_database')
 #client = InfluxDBClient(host='mydomain.com', port=8086, username='myuser', password='mypass' ssl=True, verify_ssl=True)
 q = "SELECT MEAN(water_level) FROM h2o_feet GROUP BY time(1h) LIMIT 8"
 #SELECT MEAN("water_level") FROM "h2o_feet" GROUP BY "location"
-df = pd.DataFrame(dbclient.query(q, chunked=True, chunk_size=10000).get_points())
+df = pd.DataFrame(client.query(q, chunked=True, chunk_size=10000).get_points())
 print (df)
 time = df.iloc[:,1]
 mean = df.iloc[:,0]
@@ -27,5 +26,6 @@ json_body = [
             }
         ]
 dbclient.write_points(json_body)
+dbclient = InfluxDBClient(dbhost, dbport, dbuser, dbpassword, dbname)
 print("Finished writing to InfluxDB")
 print (json_body)
