270e0fc92cf51a00ba1cdf7fd032b3a8e8b32c43
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-04-12 00:40:48 +0800

    ubah

--- olahdata.py
+++ olahdata.py
@@ -9,6 +9,7 @@ dbport = 8086
 dbuser = "admin"
 dbpassword = "123456"
 dbname = "mydb"
+tbname ="tes"
 dbclient = InfluxDBClient(dbhost, dbport, dbuser, dbpassword, dbname)
 client = InfluxDBClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_database')
 #client = InfluxDBClient(host='mydomain.com', port=8086, username='myuser', password='mypass' ssl=True, verify_ssl=True)
@@ -16,7 +17,11 @@ q = "SELECT MEAN(water_level) FROM h2o_feet GROUP BY time(1h) LIMIT 8"
 #SELECT MEAN("water_level") FROM "h2o_feet" GROUP BY "location"
 df = pd.DataFrame(client.query(q, chunked=True, chunk_size=10000).get_points())
 print (df)
-time = df.iloc[:,1]
-mean = df.iloc[:,0]
-client.write_points(result, tags={'mean': pd[['mean']]}, database='NOAA_water_database', measurement='olahh2o')
+#time = df.iloc[:,1]
+#mean = df.iloc[:,0]
+timeValues  = df[ ['col1'] ]
+timeValues.index  = df[ ['col0'] ]
+dbConnDF = DataFrameClient(dbhost, dbport, dbuser, dbpassword, dbname)
+dbConnDF.write_points(dbname, tbname, timeValues, tags = tags)
+#client.write_points(result, tags={'mean': pd[['mean']]}, database='NOAA_water_database', measurement='olahh2o')
 #print(df.loc[[159220]])
