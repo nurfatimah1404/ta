0ff8d8ad7d78df6566a0b793b5bafa29d84193f5
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-04-12 01:31:53 +0800

    tes

--- olahdata.py
+++ olahdata.py
@@ -5,17 +5,17 @@ from datetime import datetime
 from influxdb import InfluxDBClient
 from influxdb import DataFrameClient
 from influxdb import DataFrameClient
-def main(host='localhost', port=8086):
+#def main(host='localhost', port=8086):
 #"""Instantiate the connection to the InfluxDB client."""
-    user = 'root'
-    password = 'root'
-    dbhost = "10.0.12.127"
-    dbport = 8086
-    dbuser = "admin"
-    dbpassword = "123456"
-    dbname = "mydb"
-    protocol = 'line'
-    tbname = "tes"
+user = 'root'
+password = 'root'
+dbhost = "10.0.12.127"
+dbport = 8086
+dbuser = "admin"
+dbpassword = "123456"
+dbname = "mydb"
+protocol = 'line'
+tbname = "tes"
     
 client = InfluxDBClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_database')
 #client = InfluxDBClient(host='mydomain.com', port=8086, username='myuser', password='mypass' ssl=True, verify_ssl=True)
@@ -29,5 +29,5 @@ timeValues = df[ ['time'] ]
 timeValues.index = df[ ['mean'] ]
 #print (timeValues)
 tags = { 'time': df[['time']], 'mean': df[['mean']] }
-dbclient = DataFrameClient(dbhost, dbport, dbuser, password, dbname)
+dbclient = DataFrameClient(dbhost, dbport, dbuser, dbpassword, dbname)
 dbConnDF.write_points(dbname, tbname, timeValues, tags = tags)
