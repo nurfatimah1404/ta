56c5fd29621f65a912ebcdcd99deb657ea605ee4
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-04-12 01:25:08 +0800

    tes

--- olahdata.py
+++ olahdata.py
@@ -3,14 +3,20 @@ import pandas as pd
 import time
 from datetime import datetime
 from influxdb import InfluxDBClient
-#client = InfluxDBClient('localhost', 8086, 'root', 'root', 'example')
-dbhost = "10.0.12.127"
-dbport = 8086
-dbuser = "admin"
-dbpassword = "123456"
-dbname = "mydb"
-tbname ="tes"
-dbclient = InfluxDBClient(dbhost, dbport, dbuser, dbpassword, dbname)
+from influxdb import DataFrameClient
+from influxdb import DataFrameClient
+def main(host='localhost', port=8086):
+"""Instantiate the connection to the InfluxDB client."""
+    user = 'root'
+    password = 'root'
+    dbhost = "10.0.12.127"
+    dbport = 8086
+    dbuser = "admin"
+    dbpassword = "123456"
+    dbname = "mydb"
+    protocol = 'line'
+    tbname = "tes"
+    
 client = InfluxDBClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_database')
 #client = InfluxDBClient(host='mydomain.com', port=8086, username='myuser', password='mypass' ssl=True, verify_ssl=True)
 q = "SELECT MEAN(water_level) FROM h2o_feet GROUP BY time(1h) LIMIT 8"
@@ -19,12 +25,9 @@ df = pd.DataFrame(client.query(q, chunked=True, chunk_size=10000).get_points())
 print (df)
 #time = df.iloc[:,1]
 #mean = df.iloc[:,0]
-timeValues = df[ ['mean'] ]
+timeValues = df[ ['time'] ]
 timeValues.index = df[ ['mean'] ]
 #print (timeValues)
 tags = { 'time': df[['time']], 'mean': df[['mean']] }
-#dbclient = InfluxDBClient(dbhost, dbport, dbuser, dbpassword, dbname)
-dbclient.write_points(dbname, tbname, str(timeValues), tags = tags)
-
-#client.write_points(result, tags={'mean': pd[['mean']]}, database='NOAA_water_database', measurement='olahh2o')
-#print(df.loc[[159220]])
+dbclient = DataFrameClient(dbhost, dbport, dbuser, password, dbname)
+dbConnDF.write_points(dbname, tbname, timeValues, tags = tags)
