7d0a2614fad28a892e16adbcc9f53a51dcc7a86d
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-04-28 22:28:05 +0800

    tes

--- olahdata.py
+++ olahdata.py
@@ -19,8 +19,8 @@ dbname = "mydb"
 protocol = 'line'
 measurement = "tes"
     
-client = DataFrameClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_database')
-client = InfluxDBClient('10.0.12.127', 8086, 'admin', '123456', 'mydb')
+clienta = DataFrameClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_database')
+clientb = InfluxDBClient('10.0.12.127', 8086, 'admin', '123456', 'mydb')
 #client = InfluxDBClient(host='mydomain.com', port=8086, username='myuser', password='mypass' ssl=True, verify_ssl=True)
 q = "SELECT MEAN(water_level) FROM h2o_feet GROUP BY time(1h) LIMIT 1"
 #SELECT MEAN("water_level") FROM "h2o_feet" GROUP BY "location"
@@ -29,7 +29,7 @@ for point in result.get_points():
     print (point)
 #client.write_points(point,'tes')
 
-df = pd.DataFrame(client.query(q, chunked=True, chunk_size=10000).get_points())
+df = pd.DataFrame(clienta.query(q, chunked=True, chunk_size=10000).get_points())
 df["time"] = pd.to_datetime(df["time"], format="%Y-%m-%dT%H:%M:%SZ")
 #df["time"].replace("T", " ").replace("Z", "")
 #result = pd.DataFrame(client.query(q, chunked=False).raw)
