81cd949740a77966e4b050245cda84ac69e748ff
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-05-08 23:44:12 +0800

    tes

--- pengiriman2.py
+++ pengiriman2.py
@@ -58,7 +58,7 @@ longitudeList = ["119.423790","231.912345"]
 # TODO : Data log reader algorithm
 
 while 1:
-    sleep(0.50)
+    sleep(0.5)
     sampleTopic = topicList[randint(0, 10)]
     sampleid = idList[randint(0, 2)]
     sampleData = str(randint(30, 100))
