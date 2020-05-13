f56a72f9336849779498644c6968fd559691707a
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-05-13 10:27:26 +0800

    rand

--- pengiriman2.py
+++ pengiriman2.py
@@ -61,7 +61,7 @@ while 1:
     sleep(3)
     sampleTopic = topicList[randint(0, 10)]
     sampleid = idList[randint(0, 2)]
-    sampleData = str(randint(30, 100))
+    sampleData = str(randint(30, 50))
     sampleLat = latitudeList[randint(0, 1)]
     sampleLong = longitudeList[randint(0, 1)]
     mqttc.publish(sampleTopic, sampleid+";"+ sampleData +";"+ sampleLat +";"+sampleLong)
