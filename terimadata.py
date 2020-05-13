e997fedbffe4d7b7a281ce923db4174c5c87364b
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-05-13 12:10:31 +0800

    tes

--- terimadata.py
+++ terimadata.py
@@ -49,7 +49,7 @@ def on_message(client, userdata, msg):
     print(longit)
     print("------------------")
     
-    if(id ="" or sample ="" or lat ="" or longit =""):
+    if(id is None or sample is None or lat is None or longit is None):
         print("Failed writing to InfluxDB")
     else:
         json_body = [
