72d868bfbf29153fcd810ab5875052a042dac8bd
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-05-17 12:06:02 +0800

    TES

--- olahdata.py
+++ olahdata.py
@@ -9,7 +9,7 @@ import requests
 import json
     
 client = InfluxDBClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_database')
-q = "SELECT * from h2o_feet LIMI 10"
+q = "SELECT * from h2o_feet LIMIT 10"
 df = pd.DataFrame(client.query(q, chunked=True, chunk_size=10000).get_points())
 print (df)
 #tes = df.to_json(orient='records')
