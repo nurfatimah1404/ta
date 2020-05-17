d2a7a922527e913f70934ef30757e00b122d8094
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-05-17 12:41:56 +0800

    tes

--- olahdata.py
+++ olahdata.py
@@ -14,15 +14,15 @@ df = pd.DataFrame(client.query(q, chunked=True, chunk_size=10000).get_points())
 print (df)
 #tes = df.to_json(orient='records')
 #print (tes)
-timeValues  = df[ ['time'] ]
-tags        = { 'col1': df[['level']], 'col2': df[['level description']], 'col3':df[['location']], 'col4':df[['water_level']] }
+#timeValues  = df[ ['time'] ]
+#tags        = { 'col1': df[['level']], 'col2': df[['level description']], 'col3':df[['location']], 'col4':df[['water_level']] }
 
-dbConnDF = DataFrameClient('10.0.12.127', 8086, 'admin', '123456', 'mydb')
-dbConnDF.write_points('mydb', 'olah', timeValues, tags)
+#dbConnDF = DataFrameClient('10.0.12.127', 8086, 'admin', '123456', 'mydb')
+#dbConnDF.write_points('mydb', 'olah', timeValues, tags)
 #timeValues.index  = df[ ['col0'] ]
 #timeValues  = df[ ['mean'] ]
 #timeValues.index  = df[ ['time'] ]
 #dbConnDF = DataFrameClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_database')
 #dbConnDF.write_points('NOAA_water_database', 'olahdat', timeValues)
 #print("Finished writing to InfluxDB")
-#client.write_points(tes, measurements='olah', protocol='line')
+client.write_points(df,'olah', protocol='line', time_precision="'s'")
