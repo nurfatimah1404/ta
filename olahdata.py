41ff7b4ca2232aaccf1791f2b48196b6cc5f8219
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-05-17 12:12:25 +0800

    json

--- olahdata.py
+++ olahdata.py
@@ -12,11 +12,11 @@ client = InfluxDBClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_data
 q = "SELECT * from h2o_feet LIMIT 10"
 df = pd.DataFrame(client.query(q, chunked=True, chunk_size=10000).get_points())
 print (df)
-#tes = df.to_json(orient='records')
-#print (tes)
+tes = df.to_json(orient='records')
+print (tes)
 #timeValues  = df[ ['mean'] ]
 #timeValues.index  = df[ ['time'] ]
 #dbConnDF = DataFrameClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_database')
 #dbConnDF.write_points('NOAA_water_database', 'olahdat', timeValues)
 #print("Finished writing to InfluxDB")
-client.write_points(df, 'olah', protocol='line')
+client.write_points(tes, measurements='olah', protocol='line')
