7b3a0e1f1ce90e9ef79adb3b623cc5ff117c25fb
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-05-28 02:40:41 +0800

    tes

--- outlier.py
+++ outlier.py
@@ -14,7 +14,7 @@ for data_point in current_data.get_points():
     elements = numpy.array(data_point['value'])
     mean = numpy.mean(elements, axis=0)
     sd = numpy.std(elements, axis=0)
-    final_list = [x for x in arr if (x > mean - 2 * sd)
+    final_list = [x for x in data_point['value'] if (x > mean - 2 * sd)
     final_list = [x for x in final_list if (x < mean + 2 * sd)]
     print(final_list)
 
