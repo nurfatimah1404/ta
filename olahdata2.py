c1d8bebd3af1b63b29222c7616f126fe020f4fc9
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-05-30 05:37:43 +0800

    tes

--- olahdata2.py
+++ olahdata2.py
@@ -6,7 +6,7 @@ from influxdb import InfluxDBClient
 from influxdb import DataFrameClient
 import requests   
 client = InfluxDBClient('10.0.12.127', 8086, 'admin', '123456', 'NOAA_water_database')
-current_data = client.query("SELECT MEAN(water_level) FROM h2o_feet GROUP BY time(1h) LIMIT 1")
+current_data = client.query("SELECT MEAN(water_level) FROM h2o_feet GROUP BY time(1h) LIMIT 3")
 list_current_data = list(current_data.get_points())
 for data_point in current_data.get_points():
     data_to_write = [
