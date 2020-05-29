3c39293cb9a7e4aa4793d26e7269a84c8a42e8b9
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-05-30 05:41:47 +0800

    tes

--- outlier.py
+++ outlier.py
@@ -16,7 +16,7 @@ current_data = clientx.query("SELECT value FROM PM_10 where time<='2020-05-09 00
 list_current_data = list(current_data.get_points())
 print(list_current_data)
 for data_point in current_data.get_points():
-    print(data_point['value'])
+    print(data_point['value'][0])
 # generate univariate observations
     data = (data_point['value'])
     # calculate summary statistics
