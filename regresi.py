c7fd49f47c6029e6888a519cc5d91a594048a491
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-06-17 15:08:44 +0800

    ts

--- regresi.py
+++ regresi.py
@@ -6,8 +6,8 @@ from influxdb import InfluxDBClient
 from statsmodels.graphics.tsaplots import plot_pacf
 from statsmodels.graphics.tsaplots import plot_acf
 from scipy.stats import linregress
-client = InfluxDBClient('10.0.12.127', 8086, 'admin', '123456', 'mydb')
-h2O = client.query('SELECT mean("temperature") AS "h2O_temp" FROM "mydb"."autogen"."h2o_temperature" LIMIT 10')
+client = InfluxDBClient('10.0.12.127', 8086, 'admin', '123456')
+h2O = client.query('SELECT mean("degrees") AS "h2O_temp" FROM "NOAA_water_database"."autogen"."h2o_temperature"  GROUP BY time(12h) LIMIT 60')
 h2O_points = [p for p in h2O.get_points()]
 h2O_df = pd.DataFrame(h2O_points)
 h2O_df['time_step'] = range(0,len(h2O_df['time']))
