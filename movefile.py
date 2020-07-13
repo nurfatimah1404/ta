f792609b79274ed773260b0ebb51cf386aa5e4a2
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-07-13 17:49:33 +0800

    slice

--- movefile.py
+++ movefile.py
@@ -3,8 +3,8 @@ import os
 #import glob
 import glob
 
-source = '/home/data/post'
-dst = '/home/data/mvfile'
+source = '/home/data/post/'
+dst = '/home/data/mvfile/'
 files = glob.iglob(os.path.join(source, "*.csv"))
 #print(files)
 for file in files:
