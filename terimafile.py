bbeba54ae61cafbe7dc0031e05fac978fcd88a9e
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-08-29 16:25:08 +0800

    tes

--- terimafile.py
+++ terimafile.py
@@ -62,7 +62,7 @@ def getAverageRahmad():
     measurement2 = request.args.get('sensor')
     clientx2 = InfluxDBClient('182.23.82.22', 8086, 'admin', '123456', 'mydb')
     query2  = "SELECT MEAN(value) FROM {} where id='{}' AND time >='2020-08-25 09:00:00' AND time <='2020-08-25 13:49:19'group by time(1h)".format(measurement2, idSensor2)
-    data2  = clientx2.query2(query2)
+    data2  = clientx2.query(query2)
     list_current_data2 = list(data2.get_points())
     print(list_current_data2)
     return jsonify(list_current_data2)
