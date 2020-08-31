d94fa1307a02f1b8a504b6b3113105ccb5a63d8e
nurfatimah1404 <imanurfatimah147@gmail.com>
2020-08-31 23:30:01 +0800

    tes map

--- terimafile.py
+++ terimafile.py
@@ -72,7 +72,7 @@ def getFau():
     idSensor2 = request.args.get('id')
     measurement2 = request.args.get('sensor')
     clientx2 = InfluxDBClient('182.23.82.22', 8086, 'admin', '123456', 'mydb')
-    query2  = "SELECT * FROM {} where id='{}' AND time >='2020-08-26 18:00:00' AND time <='2020-08-26 20:00:19'group by time(1h)".format(measurement2, idSensor2)
+    query2  = "SELECT * FROM {} where id='{}' ".format(measurement2, idSensor2)
     data2  = clientx2.query(query2)
     list_current_data2 = list(data2.get_points())
     print(list_current_data2)
